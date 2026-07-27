5761e7270bfe6efb05ec8f64e78225cde15b5b96
Ayush <ayush.ag56648@gmail.com>
2026-07-28 01:58:39 +0530

    force delete mysql and fix config warnings

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Allows production builds to successfully complete even if type errors exist
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Allows production builds to complete even if ESLint errors exist
-    ignoreDuringBuilds: true,
   },
 };
 
